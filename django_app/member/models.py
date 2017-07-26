035a4a9a0b2c4da6ee06921bea3803e8ca3f42db
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-07-27 01:29:54 +0900

    member model 작성

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+#   pk
+# 	email(id)		EmailField					unique=true
+# 	password
+# 	nickname		CharField					unique=true, max_length=20
+# 	profile_img		CustomImageField				upload_to=user, blank=true
+# 	gender			CharField					max_length=1, choices=F/M, null=true
+# 	birth_year		IntegerField
+# 	birth_month		IntegerField
+# 	birth_day		IntegerField
+# 	hobby			CharField
+# 	region			CharField
+# 	group			ManyToManyField(Group)
+
+from utils.fields import CustomImageField
+
+
+class User(AbstractUser):
+    USER_GENDER_FEMALE = 'f'
+    USER_GENDER_MALE = 'm'
+    USER_GENDER_CHOICE = (
+        (USER_GENDER_FEMALE, 'Female'),
+        (USER_GENDER_MALE, 'Male'),
+    )
+
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=24, unique=True)
+    profile_img = CustomImageField(
+        upload_to='user/%Y/%m/%d/',
+        blank=True,
+        default_static_image='images/profile.png',
+    )
+    gender = models.CharField(max_length=1, null=True, blank=True, choices=USER_GENDER_CHOICE)
+    birth_year = models.IntegerField(validators=[MaxValueValidator(9999)])
+    birth_month = models.IntegerField(validators=[MaxValueValidator(12)])
+    birth_day = models.IntegerField(validators=[MaxValueValidator(31)])
+    hobby = models.CharField(max_length=100, blank=True, null=True)
+    region = models.CharField(max_length=100, blank=True, null=True)
+    group = models.ManyToManyField('Group')
+
+    def __str__(self):
+        return self.nickname
