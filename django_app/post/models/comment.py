afb969bfef0817099c05f460c8b2b6fa9928a868
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-07-27 12:46:57 +0900

    comment models 작성

--- django_app/post/models/comment.py
+++ django_app/post/models/comment.py
@@ -1,5 +1,11 @@
 from django.db import models
+from config.settings import base
+from post.models import Post
 
 
 class Comment(models.Model):
-    pass
+    post = models.ForeignKey(Post)
+    author = models.ForeignKey(base.AUTH_USER_MODEL)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
