552848286914169eed03397375d55e0af70ae55f
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-08-01 13:17:07 +0900

    group models 오타수정

--- django_app/group/models/group.py
+++ django_app/group/models/group.py
@@ -36,7 +36,7 @@ class Group(models.Model):
         related_name='like_groups',
     )
     created_date = models.DateTimeField(auto_now_add=True)
-    modified_d_date = models.DateTimeField(auto_now=True)
+    modified_date = models.DateTimeField(auto_now=True)
     lat = models.FloatField()
     lng = models.FloatField()
 
