ea7b711e287175f5fdc367a23515bf13f8854424
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-08-01 15:27:34 +0900

    AuthorIsRequestUser 추가 작성

--- django_app/utils/permissions/__init__.py
+++ django_app/utils/permissions/__init__.py
@@ -7,3 +7,10 @@ class ObjectIsRequestUser(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj == request.user
+
+
+class AuthorIsRequestUser(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
