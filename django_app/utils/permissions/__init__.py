dd957a416bcacb25cbb363abe2eef9259d44791a
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-08-01 13:18:28 +0900

    utils/permissions 수정없지만 편집이 바뀌어서 커밋

--- django_app/utils/permissions/__init__.py
+++ django_app/utils/permissions/__init__.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 
 class ObjectIsRequestUser(permissions.BasePermission):
+    # 읽기 권한은 모두에게 허용하므로, GET, HEAD, OPTIONS 요청은 항상 허용함
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
