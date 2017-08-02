8b6d655453c47deebac8f0841fd33297d2119c9c
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-08-02 17:48:31 +0900

    logout API - Custom logout 작성

--- django_app/member/apis/logout.py
+++ django_app/member/apis/logout.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import logout as django_logout
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
+from django.utils.translation import ugettext_lazy as _
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -15,7 +18,14 @@ class Logout(APIView):
 
     def post(self, request, format=None):
         # simply delete the token to force a login
-        request.user.auth_token.delete()
-        return Response(status=status.HTTP_200_OK)
+        return self.logout(request)
 
+    def logout(self, request):
+        try:
+            request.user.auth_token.delete()
+        except (AttributeError, ObjectDoesNotExist):
+            return Response({"detail": _("제공된 토큰이 없습니다")}, status=status.HTTP_401_UNAUTHORIZED)
 
+        django_logout(request)
+
+        return Response({"detail": _("Success logout")}, status=status.HTTP_200_OK)
