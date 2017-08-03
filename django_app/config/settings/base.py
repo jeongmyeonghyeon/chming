347f42b951045fbcb1fc41b0ac8817dfe30c1fc0
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-08-03 17:44:10 +0900

    restframework JSONRenderer 설정 추가

--- django_app/config/settings/base.py
+++ django_app/config/settings/base.py
@@ -138,8 +138,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
     )
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
-
