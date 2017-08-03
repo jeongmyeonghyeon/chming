b110b0831b3a6809665d82b4a0d925d05abe4e2a
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-08-03 17:46:53 +0900

    get_queryset 함수 추가, permissions 클래스 수정

--- django_app/post/apis/post.py
+++ django_app/post/apis/post.py
@@ -1,33 +1,46 @@
 from rest_framework import generics, permissions
 
-from utils.permissions import ObjectIsRequestUser
+from utils.permissions import AuthorIsRequestUser
 from ..models import Post, Comment
+from group.models import Group
 from ..serializer import PostSerializer
 
 __all__ = (
     'PostListCreateView',
+    'PostRetrieveUpdateDestroyAPIView',
 )
 
 
 class PostListCreateView(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
 
     def perform_create(self, serializer):
-        instance = serializer.save(author=self.request.user)
-        comment_content = self.request.data.get('comment')
-        if comment_content:
-            instance.comment_set = Comment.objects.create(
-                post=instance,
-                author=instance.author,
-                content=comment_content,
-            )
-            instance.save()
+        group_pk = self.kwargs['group_pk']
+        group = Group.objects.get(pk=group_pk)
+        instance = serializer.save(author=self.request.user, group=group)
+        # comment_content = self.request.data.get('comment')
+        # if comment_content:
+        #     instance.comment_set = Comment.objects.create(
+        #         post=instance,
+        #         author=instance.author,
+        #         content=comment_content,
+        #     )
+        instance.save()
+
+    def get_queryset(self):
+        group_pk = self.kwargs['group_pk']
+        group = Group.objects.get(pk=group_pk)
+        return Post.objects.filter(group=group)
 
 
-class RetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
+class PostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PostSerializer
     permission_classes = (
         permissions.IsAuthenticatedOrReadOnly,
-        ObjectIsRequestUser,
+        AuthorIsRequestUser,
     )
+
+    def get_queryset(self):
+        group_pk = self.kwargs['group_pk']
+        group = Group.objects.get(pk=group_pk)
+        return Post.objects.filter(group=group)
