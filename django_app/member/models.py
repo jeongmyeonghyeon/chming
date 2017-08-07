3deb3aa7eadc65efa19b902ee7e4e7845ccc6229
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-08-07 22:49:30 +0900

    user 모델 대폭 수정(AbstracBaseUser, PermissionsMixin, BaseUserManager)

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.core.validators import MaxValueValidator
 from django.db import models
 
-from group.models import group
 from utils.fields import CustomImageField
 
 from django.conf import settings
@@ -10,46 +10,78 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
+from django.utils.translation import ugettext_lazy as _
 
-# User
-#   pk
-# 	email(id)		EmailField					unique=true
-# 	password
-# 	nickname		CharField					unique=true, max_length=20
-# 	profile_img		CustomImageField			upload_to=user, blank=true
-# 	gender			CharField					max_length=1, choices=F/M, null=true
-# 	birth_year		IntegerField
-# 	birth_month		IntegerField
-# 	birth_day		IntegerField
-# 	hobby			CharField
-# 	region			CharField
-# 	group			ManyToManyField(Group)
 
-class User(AbstractUser):
-    USER_GENDER_FEMALE = 'f'
-    USER_GENDER_MALE = 'm'
-    USER_GENDER_CHOICE = (
-        (USER_GENDER_FEMALE, 'Female'),
-        (USER_GENDER_MALE, 'Male'),
-    )
+class UserManager(BaseUserManager):
+    use_in_migrations = True
 
-    username = models.EmailField(unique=True)
-    nickname = models.CharField(max_length=24, blank=True, null=True)
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=100, unique=True)
+    username = models.CharField(max_length=24, unique=True)
     profile_img = CustomImageField(
         upload_to='user/%Y/%m/%d/',
         blank=True,
         default_static_image='images/profile.png',
     )
-    gender = models.CharField(max_length=1, choices=USER_GENDER_CHOICE, blank=True, null=True)
-    birth_year = models.IntegerField(validators=[MaxValueValidator(9999)], blank=True, null=True)
-    birth_month = models.IntegerField(validators=[MaxValueValidator(12)], blank=True, null=True)
-    birth_day = models.IntegerField(validators=[MaxValueValidator(31)], blank=True, null=True)
-    hobby = models.CharField(max_length=100, blank=True, null=True)
-    region = models.CharField(max_length=100, blank=True, null=True)
+    gender = models.CharField(max_length=1, default='')
+    birth_year = models.IntegerField(validators=[MaxValueValidator(9999)], default=0)
+    birth_month = models.IntegerField(validators=[MaxValueValidator(12)], default=0)
+    birth_day = models.IntegerField(validators=[MaxValueValidator(31)], default=0)
+    hobby = models.CharField(max_length=100, default='')
+    address = models.CharField(max_length=100, default='')
+    lat = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+    lng = models.DecimalField(max_digits=9, decimal_places=6, default=0)
+    is_active = models.BooleanField(_('active'), default=True)
+    is_staff = models.BooleanField(default=False)
     joined_group = models.ManyToManyField('group.Group')
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.nickname or self.email
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def __str__(self):  # __unicode__ on Python 2
+        return self.email
 
     @receiver(post_save, sender=settings.AUTH_USER_MODEL)
     def create_auth_token(sender, instance=None, created=False, **kwargs):
