d0f3f143e0d81321b608bbc891ea4a7906b1f08d
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-08-08 18:33:27 +0900

    AuthTokenSerializer(토큰과 함께 유저정보 반환) 작성, UserCreationSerializer 수정

--- django_app/member/serializer/user.py
+++ django_app/member/serializer/user.py
@@ -15,16 +15,17 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = (
             'pk',
+            'email',
             'username',
-            'nickname',
             'profile_img',
             'gender',
             'birth_year',
             'birth_month',
             'birth_day',
             'hobby',
-            'region',
-            # 'joined_group',
+            'address',
+            'lat',
+            'lng',
         )
 
 
@@ -47,30 +48,35 @@ class UserUpdateSerializer(serializers.ModelSerializer):
 
 
 class UserCreationSerializer(serializers.Serializer):
-    username = serializers.EmailField()
-    nickname = serializers.CharField(
-        max_length=100,
-    )
-    password1 = serializers.CharField(write_only=True)
+    # Custom default error messages
+    def __init__(self, *args, **kwargs):
+        super(UserCreationSerializer, self).__init__(*args, **kwargs)
+        self.fields['email'].error_messages['blank'] = '이 항목을 채워주세요.'
+        # self.fields['email'].error_messages['invalid'] = '유효한 이메일 주소가 아닙니다.'
+        self.fields['username'].error_messages['blank'] = '이 항목을 채워주세요.'
+
+    email = serializers.EmailField(error_messages={'invalid': '유효한 이메일 주소가 아닙니다.'})
+    username = serializers.CharField(max_length=100)
+    profile_img = serializers.ImageField()
+    password1 = serializers.CharField()
     password2 = serializers.CharField(write_only=True)
+    gender = serializers.CharField(max_length=1)
     birth_year = serializers.IntegerField()
     birth_month = serializers.IntegerField()
     birth_day = serializers.IntegerField()
-    hobby = serializers.CharField(
-        max_length=100,
-    )
-    region = serializers.CharField(
-        max_length=100,
-    )
+    hobby = serializers.CharField(max_length=100)
+    address = serializers.CharField(max_length=100)
+    lat = serializers.FloatField()
+    lng = serializers.FloatField()
 
-    def validate_username(self, username):
-        if User.objects.filter(username=username).exists():
-            raise serializers.ValidationError('Username already exist')
-        return username
+    def validate_email(self, email):
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError('해당 사용자 이메일은 이미 존재합니다.')
+        return email
 
     def validate(self, data):
         if data['password1'] != data['password2']:
-            raise serializers.ValidationError('Passwords didn\'t match')
+            raise serializers.ValidationError('비밀번호가 일치하지 않습니다.')
         return data
 
     def create(self, validated_data):
@@ -82,22 +88,30 @@ class UserCreationSerializer(serializers.Serializer):
         return instance
 
     def save(self, *args, **kwargs):
-        email = self.validated_data.get('username', '')
-        nickname = self.validated_data.get('nickname', '')
+        email = self.validated_data.get('email', '')
+        username = self.validated_data.get('username', '')
+        profile_img = self.validated_data.get('profile_img', '')
+        gender = self.validated_data.get('gender', '')
         password = self.validated_data.get('password1', '')
         birth_year = self.validated_data.get('birth_year', '')
         birth_month = self.validated_data.get('birth_month', '')
         birth_day = self.validated_data.get('birth_day', '')
         hobby = self.validated_data.get('hobby', '')
-        region = self.validated_data.get('region', '')
+        address = self.validated_data.get('address', '')
+        lat = self.validated_data.get('lat', '')
+        lng = self.validated_data.get('lng', '')
         user = User.objects.create_user(
-            username=email,
-            nickname=nickname,
+            email=email,
+            username=username,
+            profile_img=profile_img,
+            gender=gender,
             password=password,
             birth_year=birth_year,
             birth_month=birth_month,
             birth_day=birth_day,
             hobby=hobby,
-            region=region,
+            address=address,
+            lat=lat,
+            lng=lng,
         )
         return user
