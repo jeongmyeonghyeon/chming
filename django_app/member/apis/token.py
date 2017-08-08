6ad515ce3bdccf753090f0a9fd7377132403e81d
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-08-08 18:30:53 +0900

    token api 작성, UserSignupView 작성, LogoutView 예외 메세지 수정, UserCreationSerializer custom error_message 및 새로 작성한 User model에 맞게 수정

---
+++ django_app/member/apis/token.py
@@ -0,0 +1,29 @@
+from rest_framework import parsers, renderers
+from rest_framework.authtoken.models import Token
+from ..serializer import AuthTokenSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from member.serializer import UserSerializer
+
+__all__ = (
+    'ObtainAuthToken',
+)
+
+class ObtainAuthToken(APIView):
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,)
+    serializer_class = AuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        userserializer = UserSerializer(user)
+        return Response({'token': token.key, 'login_user_info': userserializer.data})
+
+
+
--- django_app/member/apis/user.py
+++ django_app/member/apis/user.py
@@ -1,4 +1,6 @@
-from rest_framework import generics, permissions
+from django.contrib.auth import authenticate, login
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 
 from member.serializer.user import UserCreationSerializer
 from utils.permissions import ObjectIsRequestUser
@@ -8,6 +10,8 @@ from ..models import User
 __all__ = (
     'UserListCreateView',
     'UserRetrieveUpdateDestroyView',
+
+    'UserSignupView',
 )
 
 
@@ -19,6 +23,21 @@ class UserListCreateView(generics.ListCreateAPIView):
             return UserSerializer
         elif self.request.method == 'POST':
             return UserCreationSerializer
+
+
+class UserSignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreationSerializer
+
+    # Custom Response
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        # Response를 위한 pk값
+        pk = User.objects.get(email=serializer.data['email']).pk
+        return Response({"pk": pk}, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
