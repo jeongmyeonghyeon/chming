227ffc7ea096175242b0243cbdfb4a54416e87ec
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-08-09 22:11:22 +0900

    MainGroupListView 작성

--- django_app/group/apis/group.py
+++ django_app/group/apis/group.py
@@ -4,6 +4,10 @@ from group.serializer.group import GroupSerializer
 from utils.permissions import AuthorIsRequestUser
 from ..models import Group
 
+__all__ = (
+    'MainGroupListView',
+)
+
 
 class GroupListCreateView(generics.ListCreateAPIView):
     queryset = Group.objects.all()
@@ -23,40 +27,24 @@ class GroupRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class MainGroupListView(generics.ListAPIView):
-    pass
-    # serializer_class = GroupSerializer
-    #
-    # def get_queryset(self):
-    #     # request 된 좌표값 (현재좌표)
-    #     lat1 = float(self.request.GET['lat'])
-    #     lng1 = float(self.request.GET['lng'])
-    #     distance_limit = 0.5
-    #
-    #     groups = Group.objects.iterator()
-    #     filter_group_pk_list = []
-    #     for group in groups:
-    #         distance = distance_func(lat1, lng1)
-    #         if distance > distance_limit:
-    #             filter_group_pk_list.append(group.pk)
-    #
-    #     return Group.objects.filter(pk__in=filter_group_pk_list)
+    serializer_class = GroupSerializer
 
-
-        # print('@@@@@ lat: ', lat1, ' @@@@@: lng: ', lng1)
-        #
-        # # 특정그룹의 좌표값
-        # g = Group.objects.get(pk=1)
-        # lat2 = g.lat
-        # lng2 = g.lng
-        # print('@@@@@ lat2: ', lat2, ' @@@@@: lng2: ', lng2)
-        #
-        # # 두 좌표사이의 거리 계산
-        # print('distance = ', distance_func(lat1, lng1, lat2, lng2))
-        #
-        # # 반환값...
-        # list = []
-        # if distance_func(lat1, lng1, lat2, lng2) > 0.5:
-        #     list.append(Group.objects.get(pk=1))
-        #     print(list)
-        # else:
-        #     pass
+    def get_queryset(self):
+        # query string 으로 위도, 경도, 반경을 받는다.
+        # request.GET 의 키값을 사용
+        origin_lat = float(self.request.GET['lat'])
+        origin_lng = float(self.request.GET['lng'])
+        distance_limit = float(self.request.GET['distance_limit'])
+        print('@@@@@@@@@@', origin_lat, origin_lng, distance_limit)
+        # https://docs.djangoproject.com/en/1.11/ref/models/querysets/#iterator
+        # 쿼리를 수행하여 QuerySet을 평가하고 결과에 대한 반복자를 반환합니다. ...
+        groups = Group.objects.iterator()
+        filter_group_pk_list = []
+        for group in groups:
+            distance = group.get_distance(origin_lat, origin_lng)
+            if distance < distance_limit:
+                print(distance)
+                filter_group_pk_list.append(group.pk)
+        print(filter_group_pk_list)
+        print('Count@@@@@', Group.objects.count())
+        return Group.objects.filter(pk__in=filter_group_pk_list)
