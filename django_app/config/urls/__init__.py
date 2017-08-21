f9b166289ba2951f65b3a5d3906d573868783737
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-08-21 18:18:47 +0900

    urlpatterns 수정

--- django_app/config/urls/__init__.py
+++ django_app/config/urls/__init__.py
@@ -2,7 +2,17 @@ from django.conf.urls import url, include
 
 from config.urls import urls_views, urls_apis
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+
 urlpatterns = [
     url(r'', include(urls_views)),
     url(r'^api/', include(urls_apis)),
+    url(r'^api/admin/', admin.site.urls),
 ]
+
+# /static/에 대한 요청을 STATIC_ROOT경로의 파일에서 찾는다
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# /media/에 대한 요청을 MEDIA_ROOT경로의 파일에서 찾는다
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
